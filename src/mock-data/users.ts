6fadb49c25c4032668cee5f782bba10ac81cee30
James Becker <48602730+jameskbecker@users.noreply.github.com>
2021-10-25 13:47:50 +0100

    Added User mock data

---
+++ src/mock-data/users.ts
@@ -0,0 +1,54 @@
+type Todo = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    courseId: string;
+  };
+  
+  const data: Todo[] = [
+    {
+      id: '0',
+      firstName: 'Lydia Hartley',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+    {
+      id: '1',
+      firstName: 'Elise Griffiths',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+    {
+      id: '2',
+      firstName: 'Patrick Holt',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+    {
+      id: '3',
+      firstName: 'Edward Cole',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+    {
+      id: '4',
+      firstName: 'Spencer Carroll',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+    {
+      id: '5',
+      firstName: 'Leon Sharp',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+    {
+      id: '6',
+      firstName: 'Louis Davison',
+      lastName: 'Lydia Hartley',
+      courseId: '1',
+    },
+  ];
+  
+  export default data;
+  
