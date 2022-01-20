8608711674f1383580edea957dc798e0177d75cb
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-01-20 12:45:51 +0000

    added ProjectTable component

--- src/components/pages/project-overview/ProjectOverview.tsx
+++ src/components/pages/project-overview/ProjectOverview.tsx
@@ -1,10 +1,6 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
 import { fetchAllProjects } from '../../../store/projects';
-import { FlexColumn, FlexRow } from '../../global/Flex';
 import Footer from '../../layout/Footer';
 import Layout from '../../layout/Layout';
 import NavBar, { UserModal } from '../../layout/NavBar';
@@ -12,6 +8,7 @@ import SideBar from '../../layout/SideBar';
 import Content from './Content';
 import ControlBar from './ControlBar';
 import NewProjectModal from './NewProjectModal';
+import ProjectTable from './ProjectTable';
 import ProjectTableRow from './ProjectTableRow';
 
 type Project = {
@@ -25,30 +22,9 @@ type Project = {
   memberGroupId: string;
 };
 
-const TableHeader = styled(FlexRow)`
-  display: none;
-  flex: 0 1 auto;
-  padding: 1rem;
-  font-weight: 600;
-  justify-content: flex-start;
-
-  & > * {
-    flex: 1 1;
-  }
-
-  @media screen and (min-width: 600px) {
-    display: flex;
-  }
-`;
-
 const ProjectOverview = () => {
   const [showUserModal, setShowUserModal] = useState(false);
-  const [showNewProjectModal, setShowNewProjectModal] = useState(false);
-  // const [isLoaded, setIsLoaded] = useState(false);
-  const [projects, setProjects] = useState<Project[]>([]);
-  const { showNewProject, isLoading, data } = useSelector(
-    (state: any) => state.projects
-  );
+  const { showNewProject } = useSelector((state: any) => state.projects);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -59,18 +35,6 @@ const ProjectOverview = () => {
     setShowUserModal(!showUserModal);
   };
 
-  const toggleNewProjectModal = (e: MouseEvent) => {
-    setShowNewProjectModal(!showNewProjectModal);
-  };
-
-  const getProjects = data.map((p: any, i: number) => (
-    <ProjectTableRow
-      key={i}
-      project={p}
-      toggleNewProjectModal={toggleNewProjectModal}
-    />
-  ));
-
   return (
     <Layout>
       <SideBar activePage="projects" />
@@ -80,24 +44,7 @@ const ProjectOverview = () => {
         onClick={() => setShowUserModal(false)}
         style={{ margin: '0 1rem' }}
       >
-        <FlexColumn style={{ justifyContent: 'flex-start' }}>
-          <TableHeader>
-            {/* <div>
-              <FontAwesomeIcon icon={faSquare} />
-            </div> */}
-            <div>Priority</div>
-            <div>Name</div>
-            <div>Description</div>
-            <div>Status</div>
-            <div>Date Created</div>
-            <div>Actions</div>
-          </TableHeader>
-          {isLoading === false ? (
-            <FlexColumn>{getProjects}</FlexColumn>
-          ) : (
-            <FontAwesomeIcon style={{ flex: '1 1' }} icon={faSpinner} spin />
-          )}
-        </FlexColumn>
+        <ProjectTable />
       </Content>
       <Footer />
       {showUserModal && <UserModal />}
---
+++ src/components/pages/project-overview/ProjectTable.tsx
@@ -0,0 +1,62 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { FlexColumn, FlexRow } from '../../global/Flex';
+import ProjectTableRow from './ProjectTableRow';
+
+const TableHeader = styled(FlexRow)`
+  display: none;
+  flex: 0 1 auto;
+  padding: 1rem;
+  font-weight: 600;
+  justify-content: flex-start;
+
+  & > * {
+    flex: 1 1;
+  }
+
+  @media screen and (min-width: 600px) {
+    display: flex;
+  }
+`;
+const ProjectTable = () => {
+  const { isLoading, data } = useSelector((state: any) => state.projects);
+  const getProjects = data.map((p: any, i: number) => (
+    <ProjectTableRow key={i} project={p} />
+  ));
+
+  return (
+    <FlexColumn
+      style={{
+        justifyContent: 'flex-start',
+      }}
+    >
+      <TableHeader>
+        {/* <div>
+              <FontAwesomeIcon icon={faSquare} />
+            </div> */}
+        <div>Priority</div>
+        <div>Name</div>
+        <div>Description</div>
+        <div>Status</div>
+        <div>Date Created</div>
+        <div>Actions</div>
+      </TableHeader>
+      {isLoading === false ? (
+        <FlexColumn
+          style={{
+            overflow: 'auto',
+          }}
+        >
+          {getProjects}
+        </FlexColumn>
+      ) : (
+        <FontAwesomeIcon style={{ flex: '1 1' }} icon={faSpinner} spin />
+      )}
+    </FlexColumn>
+  );
+};
+
+export default ProjectTable;
