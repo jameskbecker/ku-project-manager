6fb2e8d56a7603c914729e8b09eeb12a85150a9e
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-03-01 16:31:28 +0000

    Update AddComment.tsx

--- src/components/modals/AddComment.tsx
+++ src/components/modals/AddComment.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleInvite } from '../../store/projects';
+import { toggleAddComment } from '../../store/tasks';
 import theme from '../../theme';
 import Button from '../global/Button';
 import TextArea from '../global/input/TextArea';
@@ -9,12 +10,12 @@ import { ModalBackdrop, ModalContent, ModalFooter } from '../global/Modal';
 import Separator from '../global/Separator';
 
 const AddCommentModal = () => {
-  const [recipient, setRecipient] = useState('');
+  const [message, setMessage] = useState('');
   const dispatch = useDispatch();
 
-  const handleRecipientChange = (e: any) => setRecipient(e.target.value);
+  const handleMessageChange = (e: any) => setMessage(e.target.value);
 
-  const handleCancel = () => dispatch(toggleInvite());
+  const handleCancel = () => dispatch(toggleAddComment());
   const handleSave = () => {};
 
   return (
@@ -27,8 +28,8 @@ const AddCommentModal = () => {
         <TextArea
           label="Message"
           placeholder="Max 500 Characters"
-          value={recipient}
-          onChange={handleRecipientChange}
+          value={message}
+          onChange={handleMessageChange}
         />
 
         <Separator />
