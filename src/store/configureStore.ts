7ad54b75f683aad3607967972881627c31203e08
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-04-27 22:20:10 +0100

    deleted authSlice

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,5 +1,4 @@
 import { apiSlice } from '@kupm/features/api/apiSlice';
-import authReducer from '@kupm/features/auth/authSlice';
 import projectsReducer from '@kupm/features/projects/projectsSlice';
 import settingsReducer from '@kupm/features/settings/settingsSlice';
 import tasksReducer from '@kupm/features/tasks/tasksSlice';
@@ -10,7 +9,6 @@ export default configureStore({
     projects: projectsReducer,
     tasks: tasksReducer,
     settings: settingsReducer,
-    auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
