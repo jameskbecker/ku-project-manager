9616e360c41807e2d61dfe79c5bf62997aa584bb
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-04-27 21:42:06 +0100

    deleted dashboardSlice

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,14 +1,12 @@
 import { apiSlice } from '@kupm/features/api/apiSlice';
 import authReducer from '@kupm/features/auth/authSlice';
-import dashboardReducer from '@kupm/features/dashboard/dashboardSlice';
 import projectsReducer from '@kupm/features/projects/projectsSlice';
 import settingsReducer from '@kupm/features/settings/settingsSlice';
 import tasksReducer from '@kupm/features/tasks/tasksSlice';
-import { configureStore, MiddlewareArray } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 export default configureStore({
   reducer: {
-    dashboard: dashboardReducer,
     projects: projectsReducer,
     tasks: tasksReducer,
     settings: settingsReducer,
