9ff0a16ab2e00749be84d8ab84ab428031f8b4ea
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-04-27 22:27:37 +0100

    removed duplicate api function

--- src/features/api/projects.ts
+++ src/features/api/projects.ts
@@ -1,16 +1,4 @@
-import { getCookie } from '@kupm/utils/cookie';
 declare const BASE_URL: string;
-
-export const postProjectInvite = async (payload: any) => {
-  const userId = getCookie('kupm_user_id');
-  const res = await fetch(`${BASE_URL}/api/invites`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ...payload, userId }),
-  });
-
-  return await res.json();
-};
 
 export const getProjectActiviy = async ({ id }: any) => {
   const res = await fetch(`${BASE_URL}/api/projects/${id}/activity`);
