ce5587b600f362f882dc12124ed4d2b295011360
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-04-29 13:43:06 +0100

    moved dashboard content

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,33 @@
+import { FlexColumn } from '@kupm/common/Flex';
 import NavBar from '@kupm/common/layout/HeaderBar';
 import Layout from '@kupm/common/layout/Layout';
-import Content from '@kupm/features/dashboard/Content';
 import NotificationPanel from '@kupm/features/notificationPanel/NotificationPanel';
 import ProjectPanel from '@kupm/features/projectPanel/ProjectPanel';
 import Sidebar from '@kupm/features/sidebar/Sidebar';
 import TodoPanel from '@kupm/features/todoPanel/TodoPanel';
 import { getCookie } from '@kupm/utils/cookie';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+
+/** @todo optimise large scale displays */
+const DashboardContent = styled(FlexColumn)`
+  grid-area: content;
+  grid-row: 2 / span 3;
+
+  padding: 1rem;
+
+  /** Tablet  */
+  @media screen and (min-width: 600px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: repeat(2, minmax(0, 1fr));
+  }
+
+  /** Desktop  */
+`;
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
   const history = useHistory();
 
   useEffect(() => {
@@ -29,11 +45,11 @@ const Dashboard = () => {
       <Sidebar activePage="dashboard" />
       {/**@todo replace with context */}
       <NavBar pageName="Dashboard" />
-      <Content>
+      <DashboardContent>
         <TodoPanel />
         <NotificationPanel />
         <ProjectPanel />
-      </Content>
+      </DashboardContent>
     </Layout>
   );
 };
