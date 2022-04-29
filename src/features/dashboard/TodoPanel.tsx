2fe07f33203687a6c82ff539789581b4d628c523
James Becker <48602730+jameskbecker@users.noreply.github.com>
2022-04-29 13:27:51 +0100

    added separate TodoPanelOptions component

--- src/features/dashboard/TodoPanel.tsx
+++ src/features/dashboard/TodoPanel.tsx
@@ -1,33 +1,18 @@
-import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
-import { SecondaryButton } from '@kupm/common/Button';
 import Panel from '@kupm/common/Panel';
-import { useGetTodoQuery } from '@kupm/features/api/apiSlice';
 import TodoList from '@kupm/features/dashboard/TodoList';
 import React from 'react';
+import styled from 'styled-components';
+import TodoPanelOptions from './TodoPanelOptions';
+
+const StyledTodoPanel = styled(Panel)`
+  grid-row: span 2;
+`;
 
 const TodoPanel = () => {
-  const { refetch } = useGetTodoQuery(null);
-
-  const TodoPanelOptions = () => {
-    return (
-      <SecondaryButton
-        secondary
-        icon={faSyncAlt}
-        onClick={refetch}
-        round
-        light
-      />
-    );
-  };
-
   return (
-    <Panel
-      heading="Tasks Todo"
-      style={{ gridRow: 'span 2' }}
-      Options={TodoPanelOptions}
-    >
+    <StyledTodoPanel heading="Tasks Todo" Options={TodoPanelOptions}>
       <TodoList />
-    </Panel>
+    </StyledTodoPanel>
   );
 };
 
---
+++ src/features/dashboard/TodoPanelOptions.tsx
@@ -0,0 +1,13 @@
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { SecondaryButton } from '@kupm/common/Button';
+import { useGetTodoQuery } from '@kupm/features/api/apiSlice';
+import React from 'react';
+
+const TodoPanelOptions = () => {
+  const { refetch } = useGetTodoQuery(null);
+  return (
+    <SecondaryButton secondary icon={faSyncAlt} onClick={refetch} round light />
+  );
+};
+
+export default TodoPanelOptions;
